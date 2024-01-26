f03cee763a7342f39232c79455f7b71030c691cc
Matthias Jaenicke <jaenicke@fzi.de>
2024-01-26 22:40:01 +0100

    Added Jolt Phsyics

---
+++ godot/scenes/game/SpotLight.gd
@@ -0,0 +1,17 @@
+extends SpotLight3D
+
+var velocity : Vector2
+var speed_mult = 0.01
+
+var base_rotation
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	base_rotation = rotation_degrees
+	velocity = Vector2(1, 0)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	rotate_x(velocity.x * speed_mult)
